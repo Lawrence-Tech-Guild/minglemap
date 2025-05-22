c73e9415844b0efba8b15f833878b593158680da
Ted Kihm <ted@euphemystic.com>
2025-05-22 08:36:04 -0500

    chore: update docker-compose and settings for improved configuration

--- minglemap/settings.py
+++ minglemap/settings.py
@@ -121,3 +121,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Where collectstatic will put files inside the container
+STATIC_ROOT = BASE_DIR / "staticfiles"
